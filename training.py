d111b40fb2ddb039d3f88dd57b1b47aa3aeb10ea
Jorgvt <vila_tronik@hotmail.com>
2024-11-13 10:30:13 +0100

    :sparkles: added validation step

--- training.py
+++ training.py
@@ -1,10 +1,10 @@
-from jaxlib.xla_extension import buffer_to_dlpack_managed_tensor
 from INRF import INRF
 import jax
 from jax import random, numpy as jnp
 import flax.linen as nn
 import optax
 import tensorflow as tf
+tf.config.set_visible_devices([], device_type="GPU")
 from keras.datasets.mnist import load_data
 
 from paramperceptnet.training import create_train_state
@@ -13,8 +13,6 @@ from paramperceptnet.training import create_train_state
 (X_train, Y_train), (X_test, Y_test) = load_data()
 X_train = X_train[...,None]/255.
 X_test = X_test[...,None]/255.
-# Y_train = Y_train[...,None]
-# Y_test = Y_test[...,None]
 
 dst_train = tf.data.Dataset.from_tensor_slices((X_train, Y_train))
 dst_test = tf.data.Dataset.from_tensor_slices((X_test, Y_test))
@@ -22,6 +20,8 @@ dst_test = tf.data.Dataset.from_tensor_slices((X_test, Y_test))
 dst_train_rdy = dst_train.shuffle(buffer_size=50, seed=42, reshuffle_each_iteration=True)\
                          .batch(32, num_parallel_calls=tf.data.AUTOTUNE)\
                          .prefetch(1)
+dst_test_rdy = dst_test.batch(32, num_parallel_calls=tf.data.AUTOTUNE)\
+                        .prefetch(1)
 
 ## Model
 class Model(nn.Module):
@@ -41,14 +41,51 @@ def train_step(state, batch):
         return optax.softmax_cross_entropy_with_integer_labels(pred, Y).mean()
     loss, grads =  jax.value_and_grad(loss_fn)(state.params)
     state = state.apply_gradients(grads=grads)
+    metrics_updates = state.metrics.single_from_model_output(loss=loss)
+    metrics = state.metrics.merge(metrics_updates)
+    state = state.replace(metrics=metrics)
     return loss, state
+
+@jax.jit
+def compute_metrics(state, batch):
+    """Obtaining the metrics for a given batch."""
+    X, Y = batch
+    def loss_fn(params):
+        pred = state.apply_fn({"params":params}, X)
+        return optax.softmax_cross_entropy_with_integer_labels(pred, Y).mean()
+
+    metrics_updates = state.metrics.single_from_model_output(loss=loss_fn(state.params))
+    metrics = state.metrics.merge(metrics_updates)
+    state = state.replace(metrics=metrics)
+    return state
+
+## Initialize metrics
+metrics_history = {'train_loss':[], 'val_loss':[]}
 
 ## Training Loop
 epochs = 10
-i = 0
 for epoch in range(epochs):
+    ## Training ##
     for batch in dst_train_rdy.as_numpy_iterator():
         loss, state = train_step(state, batch)
-        if i % 50 == 0: print(loss)
-        i += 1
 
+    ## Log the metrics
+    for name, value in state.metrics.compute().items():
+        metrics_history[f"train_{name}"].append(value)
+
+    ## Empty the metrics
+    state = state.replace(metrics=state.metrics.empty())
+
+    ## Validation ##
+    for batch in dst_test_rdy.as_numpy_iterator():
+        state = compute_metrics(state, batch)
+
+    ## Log the metrics
+    for name, value in state.metrics.compute().items():
+        metrics_history[f"val_{name}"].append(value)
+
+    ## Empty the metrics
+    state = state.replace(metrics=state.metrics.empty())
+
+    ## Logging ##
+    print(f"Epoch {epoch} -> [Train] Loss: {metrics_history['train_loss'][-1]} | [Val] Loss: {metrics_history['val_loss'][-1]}")
